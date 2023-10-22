0b86c95515e44d94fbf864843c1c1a2145d9ad74
heesane <tees3359@gmail.com>
2023-10-22 19:13:50 +0900

    inf

---
+++ javaspr/src/main/java/hhs/study/javaspr/JavasprApplication.java
@@ -0,0 +1,13 @@
+package hhs.study.javaspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavasprApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavasprApplication.class, args);
+    }
+
+}
