ae99d2d406742017bfdde310ce0657c9999d3a67
heesane <tees3359@gmail.com>
2024-03-27 16:50:52 +0900

    chore : 스프링 핵심 원리 이해 - 기본편 프로젝트 세팅

---
+++ infinite/src/main/java/inflearn2/heesane/InfiniteApplication.java
@@ -0,0 +1,13 @@
+package inflearn2.heesane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfiniteApplication.class, args);
+	}
+
+}
---
+++ infinite/src/test/java/inflearn2/heesane/InfiniteApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn2.heesane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiniteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
