92c1b3611c7b880fd34542b76f479a7707704eb4
heesane <tees3359@gmail.com>
2024-03-28 17:22:45 +0900

    feat : Order Domain Test 작성

--- infinite/src/main/java/inflearn2/heesane/discount/RateDiscountPolicy.java
+++ infinite/src/main/java/inflearn2/heesane/discount/RateDiscountPolicy.java
@@ -1,4 +1,16 @@
 package inflearn2.heesane.discount;
 
-public class RateDiscountPolicy {
+import inflearn2.heesane.member.Grade;
+import inflearn2.heesane.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private final int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price){
+        if(member.getGrade() == Grade.VIP){
+            return price * discountPercent/100;
+        }else return 0;
+
+    }
 }
---
+++ infinite/src/test/java/inflearn2/heesane/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,35 @@
+package inflearn2.heesane.discount;
+
+import inflearn2.heesane.member.Grade;
+import inflearn2.heesane.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    public void discount(){
+        Member member = new Member(1L, "heesane", Grade.VIP);
+
+        int discountPrice = discountPolicy.discount(member, 10000);
+
+        assertThat(discountPrice).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
+    public void discount2(){
+        Member member = new Member(2L, "heesane", Grade.BASIC);
+
+        int discountPrice = discountPolicy.discount(member, 10000);
+
+        assertThat(discountPrice).isEqualTo(0);
+    }
+}
