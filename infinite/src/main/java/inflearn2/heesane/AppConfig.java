b926df14c5a80182e71e766d985afc904dcd3000
heesane <tees3359@gmail.com>
2024-04-01 13:53:10 +0900

    feat : Spring Container

--- infinite/src/main/java/inflearn2/heesane/AppConfig.java
+++ infinite/src/main/java/inflearn2/heesane/AppConfig.java
@@ -2,25 +2,33 @@ package inflearn2.heesane;
 
 import inflearn2.heesane.discount.DiscountPolicy;
 import inflearn2.heesane.discount.RateDiscountPolicy;
+import inflearn2.heesane.member.MemberRepository;
 import inflearn2.heesane.member.MemberService;
 import inflearn2.heesane.member.MemberServiceImpl;
 import inflearn2.heesane.member.MemoryMemberRepository;
 import inflearn2.heesane.order.OrderService;
 import inflearn2.heesane.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
-        return new MemberServiceImpl(MemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository MemberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
-        return new OrderServiceImpl(MemberRepository(), discountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
--- infinite/src/main/java/inflearn2/heesane/OrderApp.java
+++ infinite/src/main/java/inflearn2/heesane/OrderApp.java
@@ -5,12 +5,16 @@ import inflearn2.heesane.member.Member;
 import inflearn2.heesane.member.MemberService;
 import inflearn2.heesane.order.Order;
 import inflearn2.heesane.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService",OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId,"heesane",Grade.VIP);
